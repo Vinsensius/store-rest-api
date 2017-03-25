7076a721617ea4d83a8c9271f1f40ae46693fd5b
Vinsen <adrian.vinsensius@gmail.com>
2017-03-25 13:58:59 +0700

    Fixing tabs & spaces

--- models/item.py
+++ models/item.py
@@ -18,7 +18,7 @@ class ItemModel(db.Model):
  	def json(self):
  		return {'name' : self.name, 'price' : self.price}
 
- 	@classmethod
+	@classmethod
 	def find_by_name(cls, name):
 		return cls.query.filter_by(name=name).first()
 
